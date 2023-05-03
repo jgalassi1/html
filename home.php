4b1f78e602967c3f9a403f15a8de771cb3ba3b0e
Sergio Morales <smorale3@nd.edu>
2023-05-03 16:49:51 -0400

    buttons working, old index now home.php, old login now index

--- home.php
+++ home.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 
-<html lang="en">  
-  <head>
+<html lang="en">
+<!--once we figure out blocks on flask or php we use "extends header.html" here-->
+
+<head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
@@ -10,28 +12,55 @@
         Iron Manor
     </title>
     <nav class="navbar">
-      <a href="https://google.com" class="btn-logo">
+      <a href="home.php" class="btn-logo">
         <img src="Screen Shot 2023-04-27 at 2.04.07 AM.png" alt="homejpg" width=45px; height=40px; />
       </a>
       <a href="mealplans.html" class="btn-warning">
         Explore Meal Plans
       </a>
-      <a href="login.html" class="btn-warning">
+      <a href="workoutplans.html" class="btn-warning">
         Explore Workout Plans
       </a>
     </nav>
     <!-- <div class="navbar">
         <button type="button" class="btn-logo">
-          <link  href="https://google.com">
+            <link href="home.php">
             <img src="Screen Shot 2023-04-27 at 2.04.07 AM.png" alt="homejpg" width=45px; height=40px; />
         </button>
         <button type="button" class="btn btn-warning">
-          <link href="mealplans.html">
             Explore Meal Plans
         </button>
         <button href="login.html" type="button" class="btn btn-warning">
             Explore Workout Plans
-        </button> -->
-    </div>
+        </button>
+    </div> -->
 </head>
+<hr>
+<hr>
+<hr>
+
+<body>
+    <?php include("login.php"); ?>
+    <img src="Screen Shot 2023-04-27 at 2.05.37 AM.png" alt="welcomejpg" class="welcome" />
+    <hr>
+    <h1>
+        Welcome to the House of Gainz
+    </h1>
+    <div class="columns-container">
+        <div class="left-column">
+            <h2>Feed</h2> <br>
+            <?php include('feed.php'); ?>
+        </div>
+        <div class="center-column">
+            <h2>Lifts</h2> <br>
+        </div>
+        <div class="right-column">
+            <h2>Fuel</h2>
+            <?php include('display_meals.php'); ?>
+        </div>
+    </div>
+
+
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,62 +1,29 @@
 <!DOCTYPE html>
 
-<html lang="en">
-<!--once we figure out blocks on flask or php we use "extends header.html" here-->
-
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+<html lang="en">  
+  <!--once we figure out blocks on flask or php we use "extends header.html" here-->
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>
-        Iron Manor
+      Iron Manor
     </title>
-    <div class="navbar">
-        <button type="button" class="btn-logo">
-            <img src="Screen Shot 2023-04-27 at 2.04.07 AM.png" alt="homejpg" width=45px; height=40px; />
-        </button>
-        <button type="button" class="btn btn-warning">
-            Feed
-        </button>
-        <button type="button" class="btn btn-warning">
-            Explore Meal Plans
-        </button>
-        <button type="button" class="btn btn-warning">
-            Explore Workout Plans
-        </button>
-        <button type="button" class="btn btn-outline-warning">
-            Login
-        </button>
-        <!---Here We Could also do -->
+  <body>
+    <div class="login-container"> 
+        <h1>Iron Manor</h1>
+        <h3 style="text-align: center;">Greatness Awaits</h3>
+        <form>
+            <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Username/Email</label>
+            </div>
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+            </div>
+            <button class="w-100 btn-lg btn-outline-warning" type="submit">Sign in</button>
+            <button class="w-100 btn-lg btn-warning"  type="submit">Create Account</button>
+        </form>
     </div>
-</head>
-<hr>
-<hr>
-<hr>
-<!--extends or somethings similar-->
-
-<body>
-    <?php include("login.php"); ?>
-    <img src="Screen Shot 2023-04-27 at 2.05.37 AM.png" alt="welcomejpg" class="welcome" />
-    <hr>
-    <h1>
-        Welcome to the House of Gainz
-    </h1>
-    <div class="columns-container">
-        <div class="left-column">
-            <h2>Feed</h2> <br>
-            <?php include('feed.php'); ?>
-        </div>
-        <div class="center-column">
-            <h2>Lifts</h2> <br>
-        </div>
-        <div class="right-column">
-            <h2>Fuel</h2>
-            <?php include('display_meals.php'); ?>
-        </div>
-    </div>
-
-
-</body>
-
-</html>
+  </body>
